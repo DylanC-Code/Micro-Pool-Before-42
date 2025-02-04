a42f6f819b356dbfdf0136a32cca0034805b9483
Dylan Castor <dylanc.code@gmail.com>
2025-02-04 11:55:22 +0100

    Day 4 done ✅

--- day 4/day4.c
+++ day 4/day4.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-const char *reverse(char *str)
+char *copy_string(char *source, char *destination)
 {
-    printf("String to reverse: %s\n", str);
+    int left_index = 0;
+    int write_index = strlen(source);
+
+    while (left_index < write_index)
+    {
+        destination[left_index] = source[left_index];
+        destination[write_index] = source[write_index];
+
+        left_index++;
+        write_index--;
+    }
+
+    return destination;
+}
+
+void copy_string_process(char *str_to_copy)
+{
+    size_t source_length = strlen(str_to_copy) + 1;
+    char *destination = malloc(source_length);
+    char *result = copy_string(str_to_copy, destination);
+
+    printf("The string '%s' has been copied on '%s'.\n", str_to_copy, result);
+}
+
+char *reverse(char *str)
+{
 
     int length = strlen(str);
     int left_index = 0, write_index = length - 1;
@@ -19,15 +45,26 @@ const char *reverse(char *str)
         write_index--;
     }
 
-    printf("Reversed string: %s\n", str);
-
     return str;
+}
+
+void reverse_process(char *str_to_reverse)
+{
+    printf("String to reverse: %s\n", str_to_reverse);
+
+    char *new_str = malloc(strlen(str_to_reverse) + 1);
+    new_str = copy_string(str_to_reverse, new_str);
+    char *reversed_str = reverse(new_str);
+
+    printf("Reversed string: %s\n", reversed_str);
 }
 
 int main(int argc, char *argv[])
 {
     char *str = argv[1];
-    reverse(str);
+
+    copy_string_process(str);
+    reverse_process(str);
 
     return 0;
 }

e6b93ba6ac43c653568fb121733b1dfdc677ba5b
Dylan Castor <dylanc.code@gmail.com>
2025-02-05 09:09:07 +0100

    Day 5 done ✅

--- day 5/day5.c
+++ day 5/day5.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 int integer_between_0_and_100()
 {
@@ -18,7 +19,7 @@ int *generate_random_integers(int *size)
 
     return arr;
 }
-
+    
 void random_integers_process()
 {
     int number_of_integers = integer_between_0_and_100();
@@ -32,12 +33,63 @@ void random_integers_process()
     }
 
     printf("\n");
+    free(integers);
 }
 
-int main()
+typedef struct
+{
+    char letter;
+    int times;
+} Occurrence;
+
+Occurrence *count_occurrence(char *text, Occurrence occurrences[])
+{
+    int length = strlen(text);
+
+    for (int char_index = 0; char_index < length; char_index++)
+    {
+        for (int occurrence_index = 0; occurrence_index < 26; occurrence_index++)
+        {
+            Occurrence *occurrence = &occurrences[occurrence_index];
+
+            if (occurrence->letter == text[char_index])
+            {
+                occurrence->times++;
+                break;
+            }
+            else if (occurrence->times == 0)
+            {
+                occurrence->letter = text[char_index];
+                occurrence->times = 1;
+
+                break;
+            }
+        }
+    }
+}
+
+void count_occurrences_process(char *text)
+{
+    Occurrence occurrences[26] = {0};
+    count_occurrence(text, occurrences);
+
+    for (int occurrence_index = 0; occurrence_index < 26; occurrence_index++)
+    {
+        Occurrence *occurrence = &occurrences[occurrence_index];
+
+        if (occurrence->times > 0)
+        {
+            printf("Letter '%c' occurs %d times.\n", occurrence->letter, occurrence->times);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
 {
     srand(time(NULL));
+
     random_integers_process();
+    count_occurrences_process(argv[1]);
 
     return 0;
 }

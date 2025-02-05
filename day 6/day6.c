ccd958fc85a45574c1d173ad5196506186441d65
Dylan Castor <dylanc.code@gmail.com>
2025-02-05 16:38:47 +0100

    Day 6 done✅

--- day 6/day6.c
+++ day 6/day6.c
@@ -2,6 +2,7 @@
 #include <regex.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
 struct Person
 {
@@ -17,7 +18,8 @@ int check_name(char *name)
 
     if (regcomp(&regex, pattern, REG_EXTENDED) != 0)
     {
-        perror("Checking name regex compilation error.");
+        fprintf(stderr, "Error: Failed to compile regex for name validation.\n");
+        return 0;
     };
 
     int result = regexec(&regex, name, 0, NULL, 0);
@@ -30,7 +32,7 @@ void set_name(struct Person *person)
 {
     char name[64];
 
-    printf("Enter your name: ");
+    printf("Enter a name: ");
     fgets(name, 64, stdin);
     name[strcspn(name, "\n")] = '\0';
 
@@ -41,42 +43,47 @@ void set_name(struct Person *person)
     }
     else
     {
-        printf("Invalid name, please retry only with letters and without more 63 characters.\n", name);
+        printf("Invalid name, please retry only with letters and less than 63 characters.\n", name);
         set_name(person);
     }
 }
 
 void set_age(struct Person *person)
 {
-    int user_input;
+    fflush(stdout);
+    char user_input[5];
+    int age;
 
-    printf("Enter your age: ");
-    scanf("%d", &user_input);
+    printf("Enter an age: ");
+    fgets(user_input, 5, stdin);
+    sscanf(user_input, "%d", &age);
 
-    if (user_input <= 0)
+    if (age <= 0)
     {
-        printf("The age must be greater than 0.\n");
+        printf("\nThe age must be greater than 0.\n");
         return set_age(person);
     }
-    else if (user_input > 110)
+    else if (age > 110)
     {
-        printf("There is no chance.\n");
+        printf("\nThere is no chance.\n");
         return set_age(person);
     }
 
-    person->age = user_input;
+    person->age = age;
 }
 
 void set_size(struct Person *person)
 {
     float user_input;
 
-    printf("Enter your size:");
+    printf("Enter a size: ");
     scanf("%f", &user_input);
+    while ((getchar()) != '\n')
+        ;
 
-    if (user_input > 150)
+    if (user_input > 220)
     {
-        printf("If its true, go to the gym!\n");
+        printf("Lier!\n");
         return set_size(person);
     }
     else if (user_input < 1)
@@ -88,14 +95,43 @@ void set_size(struct Person *person)
     person->size = truncf(user_input * 100) / 100;
 }
 
+#define GREEN "\033[1;32m"
+#define RESET "\033[0m"
+#define LINE "----------------------------------------"
+
+void print_saved_person(struct Person *person)
+{
+    printf("\n%s\n", LINE);
+    printf(GREEN "✅ Successfully saved !\n" RESET);
+    printf("📌 Name     : %s\n", person->name);
+    printf("🎂 Age     : %d \n", person->age);
+    printf("📏 Size  : %.2f cm\n", person->size);
+    printf("%s\n\n", LINE);
+}
+
+void save_person(struct Person *person)
+{
+    FILE *fptr;
+
+    fptr = fopen("people.csv", "a");
+    fprintf(fptr, "%s,%d,%.2f;", person->name, person->age, person->size);
+    fclose(fptr);
+
+    print_saved_person(person);
+}
+
 int main()
 {
-    struct Person person;
 
-    // set_name(&person);
-    // set_age(&person);
-    set_size(&person);
-    save_person(&person);
+    while (1)
+    {
+        struct Person person;
+
+        set_name(&person);
+        set_age(&person);
+        set_size(&person);
+        save_person(&person);
+    }
 
     return 0;
 }

2d7b4c66d22275b187703e612b771ca9d0a73c9e
Dylan Castor <dylanc.code@gmail.com>
2025-02-05 14:17:37 +0100

    Day 6: set size implemented

--- day 6/day6.c
+++ day 6/day6.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <regex.h>
 #include <string.h>
+#include <math.h>
 
 struct Person
 {
     char name[64];
     int age;
-    double taille;
+    float size;
 };
 
 int check_name(char *name)
@@ -66,12 +67,35 @@ void set_age(struct Person *person)
     person->age = user_input;
 }
 
+void set_size(struct Person *person)
+{
+    float user_input;
+
+    printf("Enter your size:");
+    scanf("%f", &user_input);
+
+    if (user_input > 150)
+    {
+        printf("If its true, go to the gym!\n");
+        return set_size(person);
+    }
+    else if (user_input < 1)
+    {
+        printf("Stop kidding.\n");
+        return set_size(person);
+    }
+
+    person->size = truncf(user_input * 100) / 100;
+}
+
 int main()
 {
     struct Person person;
 
-    set_name(&person);
-    set_age(&person);
+    // set_name(&person);
+    // set_age(&person);
+    set_size(&person);
+    save_person(&person);
 
     return 0;
 }
